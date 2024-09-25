07341e3c977db6a5b3ad4e6881b24f7a85aa99d2
w1n7er <134517334+w1n7er87@users.noreply.github.com>
2024-09-25 18:56:59 +0100

    Revert "fix prefixes and remove unauth response"
    This reverts commit 1b93d3764d9adb9f36e71e034c6b7bf2f8e848f6.

--- StreamSchedule/Program.cs
+++ StreamSchedule/Program.cs
@@ -153,8 +153,7 @@ internal class BotCore
 
         if (!firstLetters.Any()) return;
 
-        var noPrefix = msgAsCodepoints.Skip(firstLetters.Count());
-
+        var noPrefix = msgAsCodepoints.Where(x => !firstLetters.Contains(x));
         string restoredMessage = "";
         foreach (var l in noPrefix) 
         {
@@ -179,9 +178,9 @@ internal class BotCore
 
                 if (userSent.privileges < command.Privileges)
                 {
-                    //_client.SendMessage(e.ChatMessage.Channel, "✋ unauthorized action" + bypassSameMessage);
-                    //_sameMessage = !_sameMessage;
-                    //_textCommandLastUsed = DateTime.Now;
+                    _client.SendMessage(e.ChatMessage.Channel, "✋ unauthorized action" + bypassSameMessage);
+                    _sameMessage = !_sameMessage;
+                    _textCommandLastUsed = DateTime.Now;
                     return;
                 }
 
@@ -201,9 +200,9 @@ internal class BotCore
 
             if (userSent.privileges < c.MinPrivilege)
             {
-                //_client.SendMessage(e.ChatMessage.Channel, "✋ unauthorized action" + bypassSameMessage);
-                //_sameMessage = !_sameMessage;
-                //c.LastUsedOnChannel[e.ChatMessage.Channel] = DateTime.Now;
+                _client.SendMessage(e.ChatMessage.Channel, "✋ unauthorized action" + bypassSameMessage);
+                _sameMessage = !_sameMessage;
+                c.LastUsedOnChannel[e.ChatMessage.Channel] = DateTime.Now;
                 return;
             }
 
