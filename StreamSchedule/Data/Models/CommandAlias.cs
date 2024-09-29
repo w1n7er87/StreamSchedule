e94c5e280e542285a107164537bdb11b07fc7cf5
w1n7er <134517334+w1n7er87@users.noreply.github.com>
2024-09-29 09:12:14 +0100

    add command aliases

---
+++ StreamSchedule/Data/Models/CommandAlias.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StreamSchedule.Data.Models;
+
+[PrimaryKey("CommandName")]
+public class CommandAlias
+{
+    public required string CommandName {  get; set; }
+    public List<string>? Aliases { get; set; }
+}
