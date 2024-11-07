75b1eb4bb47858d8af98791cb6b80ad2a4d4d17d
w1n7er <134517334+w1n7er87@users.noreply.github.com>
2024-11-07 22:19:18 +0000

    Update Ping.cs

--- StreamSchedule/Commands/Ping.cs
+++ StreamSchedule/Commands/Ping.cs
@@ -7,14 +7,12 @@ internal class Ping : Command
     internal override string Call => "ping";
     internal override Privileges MinPrivilege => Privileges.Trusted;
     internal override string Help => "ping";
-    internal override TimeSpan Cooldown => TimeSpan.FromSeconds((int)Cooldowns.Minute);
+    internal override TimeSpan Cooldown => TimeSpan.FromSeconds((int)Cooldowns.Short);
     internal override Dictionary<string, DateTime> LastUsedOnChannel { get; set; } = [];
     internal override string[]? Arguments => null;
 
     internal override Task<CommandResult> Handle(UniversalMessageInfo message)
     {
-        bool result = BotCore.Client.Connect();
-        Console.WriteLine($"is connected: {BotCore.Client.IsConnected} connect result: {result} \n");
-        return Task.FromResult(new CommandResult("PotFriend ", false));
+        return Task.FromResult( new CommandResult("PotFriend ", false));
     }
 }
