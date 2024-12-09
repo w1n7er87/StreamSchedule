78ba3b28f75799eb339d623808e517429e1befe1
w1n7er <134517334+w1n7er87@users.noreply.github.com>
2024-12-09 18:46:45 +0000

    Update GetStream.cs

--- StreamSchedule/Commands/GetStream.cs
+++ StreamSchedule/Commands/GetStream.cs
@@ -1,4 +1,5 @@
-﻿using StreamSchedule.Data;
+﻿using System.Globalization;
+using StreamSchedule.Data;
 
 namespace StreamSchedule.Commands;
 
@@ -9,10 +10,12 @@ internal class GetStream : Command
     internal override string Help => "time until next stream on the schedule.";
     internal override TimeSpan Cooldown => TimeSpan.FromSeconds((int)Cooldowns.Long);
     internal override Dictionary<string, DateTime> LastUsedOnChannel { get; set; } = [];
-    internal override string[]? Arguments => null;
+    internal override string[] Arguments => ["s", "m", "h"];
 
     internal override Task<CommandResult> Handle(UniversalMessageInfo message)
     {
+        Commands.RetrieveArguments( Arguments, message.content, out Dictionary<string, string> usedArgs);
+        
         var next = BotCore.DBContext.Streams.Where(x => x.StreamDate >= DateOnly.FromDateTime(DateTime.UtcNow))
             .AsEnumerable()
             .OrderBy(x => new DateTime(x.StreamDate, x.StreamTime))
@@ -25,6 +28,14 @@ internal class GetStream : Command
 
         DateTime fullDate = new DateTime(next.StreamDate, next.StreamTime).ToLocalTime();
         TimeSpan span = fullDate - DateTime.Now;
-        return Task.FromResult(new CommandResult($"Next stream is in {(span.Days != 0 ? span.Days + "d " : "")}{(span.Hours != 0 ? span.Hours + "h " : "")}{span:m'm 's's '} : {next.StreamTitle}"));
+        string time = $"{(span.Days != 0 ? span.Days + "d " : "")}{(span.Hours != 0 ? span.Hours + "h " : "")}{span:m'm 's's '}";
+        
+        if (usedArgs.TryGetValue("s", out _)) time = span.TotalSeconds.ToString(CultureInfo.InvariantCulture) + " seconds";
+
+        if (usedArgs.TryGetValue("m", out _)) time = span.TotalMinutes.ToString(CultureInfo.InvariantCulture) + " minutes";
+
+        if (usedArgs.TryGetValue("h", out _)) time = span.TotalHours.ToString(CultureInfo.InvariantCulture) + " hours";
+
+        return Task.FromResult(new CommandResult($"Next stream is in {time} : {next.StreamTitle}"));
     }
 }
