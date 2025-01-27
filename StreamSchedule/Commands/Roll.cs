22de4052ede16dddc5c1bc707ee4f5a2570a2e6a
w1n7er <134517334+w1n7er87@users.noreply.github.com>
2025-01-27 19:44:51 +0000

    Update Roll.cs

--- StreamSchedule/Commands/Roll.cs
+++ StreamSchedule/Commands/Roll.cs
@@ -6,30 +6,28 @@ internal class Roll : Command
 {
     internal override string Call => "roll";
     internal override Privileges MinPrivilege => Privileges.None;
-    internal override string Help => "get random number [0 - 100], -max[n] for [0 - n], -flip for 50/50";
+    internal override string Help => "get random number [0 - n( = 100)], -flip for 50/50";
     internal override TimeSpan Cooldown => TimeSpan.FromSeconds((int)Cooldowns.Medium);
     internal override Dictionary<string, DateTime> LastUsedOnChannel { get; set; } = [];
-    internal override string[] Arguments => ["flip", "max"];
+    internal override string[] Arguments => ["flip"];
 
-    private static readonly string[] neuros = ["nwero", "hiyori", "eliv", "nuero", "newero", "wuero", "cleliv", "cluero"];
+    private static readonly string[] neuros = ["nwero", "hiyori", "eliv", "nuero", "newero", "wuero", "cleliv", "cluero", "weliv"];
 
     internal override Task<CommandResult> Handle(UniversalMessageInfo message)
     {
-        CommandResult result = new($"{neuros[Random.Shared.Next(neuros.Length)]} says: ");
-
+        CommandResult result = new($"{
+            (Random.Shared.Next(100) < 50 ? neuros[Random.Shared.Next(neuros.Length)] : BotCore.MessageCache[Random.Shared.Next(BotCore.MessageCache.Count)].Username)
+            } says: ");
+        
         string[] split = Commands.RetrieveArguments(Arguments, message.content, out Dictionary<string, string> usedArgs).Split(' ');
-
-        int maxValue = int.TryParse(split[0], out int c) ? c > 0 ? c + 1 : 101 : 101;
-
-        if (usedArgs.TryGetValue("max", out string? a))
-        {
-            maxValue = int.TryParse(a, out int b) ? b > 0 ? b + 1 : 101 : 101;
-        }
 
         if (usedArgs.TryGetValue("flip", out _))
         {
             return Task.FromResult(result + (Random.Shared.Next(100) < 50 ? "YES " : "NO "));
         }
+
+        int maxValue = int.TryParse(split[0], out int c) ? int.Clamp(c + 1, 0, int.MaxValue) : 101;
+
         return Task.FromResult(result + Random.Shared.Next(maxValue).ToString());
     }
 }
