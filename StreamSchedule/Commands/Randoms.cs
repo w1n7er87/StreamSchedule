f4e8b7582d26be3047475524b01b8fab7dc4ab0c
w1n7er <134517334+w1n7er87@users.noreply.github.com>
2025-02-24 16:19:54 +0000

    remove random words

--- StreamSchedule/Commands/Randoms.cs
+++ StreamSchedule/Commands/Randoms.cs
@@ -9,7 +9,7 @@ internal class Randoms : Command
     internal override string Help => "get random number [0 - n( = 100)]; -flip for 50/50; -w(x = 5) for words from recent messages (50 max)";
     internal override TimeSpan Cooldown => TimeSpan.FromSeconds((int)Cooldowns.Long);
     internal override Dictionary<string, DateTime> LastUsedOnChannel { get; set; } = [];
-    internal override string[] Arguments => ["flip", "w"];
+    internal override string[] Arguments => ["flip"];
 
     private static readonly string[] neuros = ["nwero", "hiyori", "eliv", "nuero", "newero", "wuero", "cleliv", "cluero", "weliv"];
 
@@ -27,21 +27,6 @@ internal class Randoms : Command
         }
 
         int maxValue = int.TryParse(split[0], out int c) ? int.Clamp(c + 1, 1, int.MaxValue) : 101;
-
-        if (usedArgs.TryGetValue("w", out string? wordCount))
-        {
-            int max = int.TryParse(wordCount, out int cc) ? int.Clamp(cc, 1, 50) : 5;
-            List<string> results = [];
-            int count = 0;
-
-            while(count < max)
-            {
-                string[] splitMessage = [.. BotCore.MessageCache[Random.Shared.Next(BotCore.MessageCache.Count)].Message.Split(' ').Where(x => !x.StartsWith('@'))];
-                results.Add(splitMessage[Random.Shared.Next(splitMessage.Length)]);
-                count++;
-            }
-            return Task.FromResult(result + string.Join(" ", results));
-        }
 
         return Task.FromResult(result + Random.Shared.Next(1, maxValue).ToString());
     }
