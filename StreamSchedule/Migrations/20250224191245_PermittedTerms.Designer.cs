277676967da58e72cfd56b809ff0eb31f0c06a0a
w1n7er <134517334+w1n7er87@users.noreply.github.com>
2025-02-24 19:16:10 +0000

    migration

--- StreamSchedule/Migrations/20250224191245_PermittedTerms.Designer.cs
+++ StreamSchedule/Migrations/20250224191245_PermittedTerms.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StreamSchedule.Data;
 
@@ -10,9 +11,11 @@ using StreamSchedule.Data;
 namespace StreamSchedule.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20250224191245_PermittedTerms")]
+    partial class PermittedTerms
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.2");
---
+++ StreamSchedule/Migrations/20250224191245_PermittedTerms.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StreamSchedule.Migrations
+{
+    /// <inheritdoc />
+    public partial class PermittedTerms : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PermittedTerms",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Term = table.Column<string>(type: "TEXT", nullable: false),
+                    Noreplace = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Anycase = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Alternative = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PermittedTerms", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PermittedTerms");
+        }
+    }
+}
