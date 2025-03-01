8090e8c5df3ad6116a05a38ab7f399942beadf5b
w1n7er <134517334+w1n7er87@users.noreply.github.com>
2025-03-01 17:48:58 +0000

    new score again

--- StreamSchedule/Userscore.cs
+++ StreamSchedule/Userscore.cs
@@ -10,16 +10,21 @@ public struct RatioScore(float r, float s)
 
 internal static class Userscore
 {
+    
     public static RatioScore GetRatioAndScore(User u)
     {
         int offline = u.MessagesOffline;
         int online = u.MessagesOnline;
+
+        int offlineSafe = Math.Max(1, offline);
+        int onlineSafe = Math.Max(1, online);
+
         int total = offline + online;
         float ratio = 0f;
 
         if (offline != 0) ratio = (float)offline / total;
 
-        float score = (MathF.Log(MathF.Max(1, offline)) - MathF.Log10(MathF.Max(1, online))) * ratio * (total / 10000.0f);
+        float score = MathF.Log10(0.01f * offlineSafe + 5f * onlineSafe) - MathF.Log10(onlineSafe / MathF.Max(1, 0.5f * total));
 
         return new RatioScore(ratio, score);
     }
