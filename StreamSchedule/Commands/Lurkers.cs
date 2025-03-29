cfee486d19f67f3e3372e43328ae776fb3dbb9f6
w1n7er <134517334+w1n7er87@users.noreply.github.com>
2025-03-29 16:38:17 +0000

    make random ping not optional

--- StreamSchedule/Commands/Lurkers.cs
+++ StreamSchedule/Commands/Lurkers.cs
@@ -10,25 +10,16 @@ internal class Lurkers : Command
     internal override string Help => "get channel lurkers";
     internal override TimeSpan Cooldown => TimeSpan.FromSeconds((int)Cooldowns.Minute);
     internal override Dictionary<string, DateTime> LastUsedOnChannel { get; set; } = [];
-    internal override string[] Arguments => ["p"];
+    internal override string[]? Arguments => null;
 
     internal override async Task<CommandResult> Handle(UniversalMessageInfo message)
     {
-        string? target = Commands.RetrieveArguments(Arguments, message.content, out Dictionary<string, string> usedArgs).Split(' ')[0];
-
-        if (!string.IsNullOrWhiteSpace(target) && target.StartsWith('@') && message.sender.Privileges >= Privileges.Trusted)
-        {
-            target = target.Replace("@", "");
-            (int, Chatter?[]?) c = await BotCore.GQLClient.GetChattersCount(message.channelID, target);
-            return new($"{c.Item1} lurkers in {target}'s chat uuh ");
-        }
-
         (int, Chatter?[]?) count = await BotCore.GQLClient.GetChattersCount(message.channelID);
-        string chatter = "";
-        if (usedArgs.TryGetValue("p", out _))
-        {
-            if (count.Item2 is not null && count.Item2.Length != 0) chatter = $", and @{count.Item2[Random.Shared.Next(count.Item2.Length)]?.Login ?? BotCore.Client.TwitchUsername} is one of them";
-        }
+
+        string chatter = (count.Item2 is not null && count.Item2.Length != 0)
+            ? $", and @{count.Item2[Random.Shared.Next(count.Item2.Length)]?.Login ?? BotCore.Client.TwitchUsername} is one of them"
+            : "";
+        
         return new($"{count.Item1} lurkers{chatter} uuh");
     }
 }
